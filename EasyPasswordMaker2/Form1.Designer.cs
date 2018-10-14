cd008487348be90f40bc7936fffe4569d7bcb764
Julian (Jules) Brown <julesbrn@gmail.com>
2018-10-13 22:45:30 -0500

    Add project files.

---
+++ EasyPasswordMaker2/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace EasyPasswordMaker2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.fName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lName = new System.Windows.Forms.TextBox();
+            this.wName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.num = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.passLength = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.fNameTip = new System.Windows.Forms.ToolTip(this.components);
+            this.lNameTip = new System.Windows.Forms.ToolTip(this.components);
+            this.wNameTip = new System.Windows.Forms.ToolTip(this.components);
+            this.numTip = new System.Windows.Forms.ToolTip(this.components);
+            this.lenTip = new System.Windows.Forms.ToolTip(this.components);
+            this.clipTip = new System.Windows.Forms.ToolTip(this.components);
+            this.fnq = new System.Windows.Forms.Button();
+            this.lnq = new System.Windows.Forms.Button();
+            this.wnq = new System.Windows.Forms.Button();
+            this.nq = new System.Windows.Forms.Button();
+            this.lq = new System.Windows.Forms.Button();
+            this.cq = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.passLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // fName
+            // 
+            resources.ApplyResources(this.fName, "fName");
+            this.fName.Name = "fName";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // outbox
+            // 
+            resources.ApplyResources(this.outbox, "outbox");
+            this.outbox.Name = "outbox";
+            this.outbox.ReadOnly = true;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lName
+            // 
+            resources.ApplyResources(this.lName, "lName");
+            this.lName.Name = "lName";
+            // 
+            // wName
+            // 
+            resources.ApplyResources(this.wName, "wName");
+            this.wName.Name = "wName";
+            this.wName.TextChanged += new System.EventHandler(this.wName_TextChanged);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // num
+            // 
+            resources.ApplyResources(this.num, "num");
+            this.num.Name = "num";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // passLength
+            // 
+            resources.ApplyResources(this.passLength, "passLength");
+            this.passLength.Maximum = new decimal(new int[] {
+            64,
+            0,
+            0,
+            0});
+            this.passLength.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.passLength.Name = "passLength";
+            this.passLength.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // fNameTip
+            // 
+            this.fNameTip.AutomaticDelay = 100;
+            this.fNameTip.AutoPopDelay = 10000;
+            this.fNameTip.InitialDelay = 100;
+            this.fNameTip.ReshowDelay = 0;
+            // 
+            // lNameTip
+            // 
+            this.lNameTip.AutomaticDelay = 100;
+            this.lNameTip.AutoPopDelay = 10000;
+            this.lNameTip.InitialDelay = 100;
+            this.lNameTip.ReshowDelay = 0;
+            // 
+            // wNameTip
+            // 
+            this.wNameTip.AutomaticDelay = 100;
+            this.wNameTip.AutoPopDelay = 10000;
+            this.wNameTip.InitialDelay = 100;
+            this.wNameTip.ReshowDelay = 0;
+            // 
+            // numTip
+            // 
+            this.numTip.AutomaticDelay = 100;
+            this.numTip.AutoPopDelay = 10000;
+            this.numTip.InitialDelay = 100;
+            this.numTip.ReshowDelay = 20;
+            // 
+            // lenTip
+            // 
+            this.lenTip.AutomaticDelay = 100;
+            this.lenTip.AutoPopDelay = 10000;
+            this.lenTip.InitialDelay = 100;
+            this.lenTip.ReshowDelay = 20;
+            // 
+            // clipTip
+            // 
+            this.clipTip.AutomaticDelay = 100;
+            this.clipTip.AutoPopDelay = 10000;
+            this.clipTip.InitialDelay = 100;
+            this.clipTip.ReshowDelay = 20;
+            // 
+            // fnq
+            // 
+            resources.ApplyResources(this.fnq, "fnq");
+            this.fnq.Name = "fnq";
+            this.fnq.UseVisualStyleBackColor = true;
+            this.fnq.Click += new System.EventHandler(this.fnq_Click);
+            // 
+            // lnq
+            // 
+            resources.ApplyResources(this.lnq, "lnq");
+            this.lnq.Name = "lnq";
+            this.lnq.UseVisualStyleBackColor = true;
+            // 
+            // wnq
+            // 
+            resources.ApplyResources(this.wnq, "wnq");
+            this.wnq.Name = "wnq";
+            this.wnq.UseVisualStyleBackColor = true;
+            // 
+            // nq
+            // 
+            resources.ApplyResources(this.nq, "nq");
+            this.nq.Name = "nq";
+            this.nq.UseVisualStyleBackColor = true;
+            // 
+            // lq
+            // 
+            resources.ApplyResources(this.lq, "lq");
+            this.lq.Name = "lq";
+            this.lq.UseVisualStyleBackColor = true;
+            // 
+            // cq
+            // 
+            resources.ApplyResources(this.cq, "cq");
+            this.cq.Name = "cq";
+            this.cq.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.cq);
+            this.Controls.Add(this.lq);
+            this.Controls.Add(this.nq);
+            this.Controls.Add(this.wnq);
+            this.Controls.Add(this.lnq);
+            this.Controls.Add(this.fnq);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.passLength);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.num);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.wName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.passLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox outbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox lName;
+        private System.Windows.Forms.TextBox wName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox num;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown passLength;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolTip fNameTip;
+        private System.Windows.Forms.ToolTip lNameTip;
+        private System.Windows.Forms.ToolTip wNameTip;
+        private System.Windows.Forms.ToolTip numTip;
+        private System.Windows.Forms.ToolTip lenTip;
+        private System.Windows.Forms.ToolTip clipTip;
+        private System.Windows.Forms.Button fnq;
+        private System.Windows.Forms.Button lnq;
+        private System.Windows.Forms.Button wnq;
+        private System.Windows.Forms.Button nq;
+        private System.Windows.Forms.Button lq;
+        private System.Windows.Forms.Button cq;
+    }
+}
+
---
+++ EasyPasswordMaker2/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyPasswordMaker2
+{
+    public partial class Form1 : Form
+    {
+        String raw = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+            //set tooltips
+            fNameTip.SetToolTip(fName, "Your first name should go here. Capitalization does not matter.");
+            fNameTip.SetToolTip(fnq, "Your first name should go here. Capitalization does not matter.");
+
+            lNameTip.SetToolTip(lName, "Your last name should go here. Capitalization does not matter.");
+            lNameTip.SetToolTip(lnq, "Your last name should go here. Capitalization does not matter.");
+
+            wNameTip.SetToolTip(wName, "The name of the website should go here. Capitalization does not matter.");
+            wNameTip.SetToolTip(wnq, "The name of the website should go here. Capitalization does not matter.");
+
+            numTip.SetToolTip(num, "This should be a number. This makes it easier to change your password without changing anything else.");
+            numTip.SetToolTip(nq, "This should be a number. This makes it easier to change your password without changing anything else.");
+
+            lenTip.SetToolTip(passLength, "This is the length of your password. Min:1, Max:64, Recommended:32");
+            lenTip.SetToolTip(lq, "This is the length of your password. Min:1, Max:64, Recommended:32");
+
+            clipTip.SetToolTip(checkBox1, "Check this for your password to be copied to your clipboard for easy pasting.");
+            clipTip.SetToolTip(cq, "Check this for your password to be copied to your clipboard for easy pasting.");
+        }
+
+        public static string GenerateSHA512String(string inputString)
+        {
+            SHA512 sha512 = SHA512Managed.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(inputString);
+            byte[] hash = sha512.ComputeHash(bytes);
+            return GetStringFromHash(hash);
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+            return result.ToString();
+        }
+
+        private static char[] digits = new char[10] {')', '!', '@', '#', '$', '%', '^', '&', '*', '('};
+
+        private static String stronger(String tmp) //This function makes the string stronger
+        {
+            StringBuilder builder = new StringBuilder(tmp); //Strings are immutable, StringBuilder is not. The performance gain is not significant, but it is good practice.
+            for (int i = 0; i < builder.Length; i++) //for each character in string.
+            {
+                if (i % 2 == 0) //if this index is even
+                {
+                    if (System.Char.IsDigit(builder[i])) //is it a number?
+                    {
+                        int tp = Convert.ToInt32(builder[i]) - 48; //convert number from character in ascii table to int
+                        builder[i] = digits[tp]; //replace the corresponding number with it's qwerty equivalent
+                    }
+                    else
+                    {
+                        builder[i] = Char.ToLower(builder[i]); //if its a character, make it lowercase.
+                    }
+                }
+                else //if this index is odd
+                {
+                    if (!System.Char.IsDigit(builder[i])) //if it is not a digit
+                    {
+                        builder[i] = Char.ToUpper(builder[i]); //make it uppercase
+                    }
+                }
+            }
+
+            return builder.ToString(); //convert it back to string and return it.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            raw = "";
+            fName.Text = fName.Text.ToLower();
+            lName.Text = lName.Text.ToLower();
+            wName.Text = wName.Text.ToLower();
+            num.Text = num.Text.ToLower(); //Make everything lowercase to ensure the user can not mess this up.
+
+            
+            Boolean isDone = false; //while loop logic boolean
+            int counter = 0;
+            while (!isDone)
+            {
+                int doneFields = 0; //keep track of the number of fields that are finished
+                //this create "a1b2c3" from "abc" and "123"
+                if (counter < fName.Text.Length ) //make sure the next character exists
+                {
+                    raw += fName.Text[counter]; //add it to the raw string
+                }
+                else
+                {
+                    doneFields++; //character does not exist, note that.
+                }
+                if (counter < lName.Text.Length)
+                {
+                    raw += lName.Text[counter];
+                }
+                else
+                {
+                    doneFields++;
+                }
+                if (counter < wName.Text.Length)
+                {
+                    raw += wName.Text[counter];
+                }
+                else
+                {
+                    doneFields++;
+                }
+                if (doneFields == 3) isDone = true; //3 is the number of textboxes. if we hit that, stop the loop.
+                counter++; //next characters
+            }
+            raw += num.Text; //The number is appended at the end
+            //outbox.Text = raw;
+
+            String hashed = GenerateSHA512String(raw); //sha512 the raw string
+
+            hashed = hashed.Substring(0,  Convert.ToInt32(passLength.Value)); //get only the length the user wants
+
+            hashed = stronger(hashed); //make the password stronger
+            
+            //outbox.Text += "\n\n\n" + hashed; //use this if you want both the raw and hashed
+            outbox.Text = hashed;
+
+            if (checkBox1.Checked) System.Windows.Forms.Clipboard.SetText(hashed); //copy to clipboard
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fnq_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
